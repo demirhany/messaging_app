8b43ca9839a7c61c30396031274c44304a40c639
Demirhan Yilal <demirhan.yilal@sakarya.edu.tr>
2024-07-05 15:02:22 +0300

    Feat: Logout endpoint has been added into Auth Rest Controller.

--- messaging_app/src/main/java/org/spring_app/messaging_app/controller/AuthenticationRestController.java
+++ messaging_app/src/main/java/org/spring_app/messaging_app/controller/AuthenticationRestController.java
@@ -1,6 +1,5 @@
 package org.spring_app.messaging_app.controller;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.spring_app.messaging_app.dto.AuthenticationRequest;
@@ -23,17 +22,17 @@ public class AuthenticationRestController {
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest authenticationRequest, HttpServletResponse httpServletResponse) {
         AuthenticationResponse response = authenticationService.authenticate(authenticationRequest);
-        Cookie cookie = new Cookie("jwtToken", response.getToken());
-        cookie.setHttpOnly(true);
-        cookie.setPath("/");
-        cookie.setMaxAge(1000 * 60 * 60 * 24 * 10);
-
-        httpServletResponse.addCookie(cookie);
+        httpServletResponse.addCookie(response.getCookie());
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/register")
     public ResponseEntity<UserDto> register(@RequestBody UserCreateRequest request) {
         return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+    @PostMapping("/logout")
+    public String logout() {
+        return "redirect:/auth/login";
     }
 }
