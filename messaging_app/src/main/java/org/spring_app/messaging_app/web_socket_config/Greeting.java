6553e894efce5c30faf918af15c9e57a8f58b265
Demirhan Yilal <demirhan.yilal@sakarya.edu.tr>
2024-07-19 21:06:00 +0300

    feat: basic websocket configurations have been added.

---
+++ messaging_app/src/main/java/org/spring_app/messaging_app/web_socket_config/Greeting.java
@@ -0,0 +1,12 @@
+package org.spring_app.messaging_app.web_socket_config;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Greeting {
+    String content;
+}
---
+++ messaging_app/src/main/java/org/spring_app/messaging_app/web_socket_config/HelloMessage.java
@@ -0,0 +1,12 @@
+package org.spring_app.messaging_app.web_socket_config;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class HelloMessage {
+    private String name;
+}
