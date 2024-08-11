25b930e70b0a8b8dbc4f73b12256d7b614158b81
Demirhan Yilal <demirhanyilal0@gmail.com>
2024-08-11 16:03:25 +0300

    Feat: Basic messaging has been implemented.

--- messaging_app/src/main/java/org/spring_app/messaging_app/web_socket_config/WebSocketConfig.java
+++ messaging_app/src/main/java/org/spring_app/messaging_app/web_socket_config/WebSocketConfig.java
@@ -19,4 +19,31 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/gs-guide-websocket");
     }
+
 }
+
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+//import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+//import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+//import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+//
+//@Configuration
+//@EnableWebSocketMessageBroker
+//public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+//
+//    @Override
+//    public void configureMessageBroker(MessageBrokerRegistry config) {
+//        // Enable a simple message broker for the "/topic" and "/queue" destinations
+//        config.enableSimpleBroker("/topic", "/queue");
+//        // Designate the "/app" prefix for message-mapping methods
+//        config.setApplicationDestinationPrefixes("/app");
+//    }
+//
+//    @Override
+//    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        // Register the "/ws" endpoint for WebSocket connections
+//        registry.addEndpoint("/ws").withSockJS();
+//    }
+//}
+
