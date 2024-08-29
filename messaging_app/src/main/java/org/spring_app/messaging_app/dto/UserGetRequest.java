e08cccc56449652a7e6095f5437429033bd670b5
Demirhan Yilal <demirhanyilal0@gmail.com>
2024-08-29 16:39:08 +0300

    Feat: UserGetRequest has been created to get users' nick.

---
+++ messaging_app/src/main/java/org/spring_app/messaging_app/dto/UserGetRequest.java
@@ -0,0 +1,19 @@
+package org.spring_app.messaging_app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.spring_app.messaging_app.entity.User;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserGetRequest {
+    private String nick;
+
+    public UserGetRequest(User user){
+        this.nick = user.getNick();
+    }
+}
