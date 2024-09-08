ea8c946a37f4bcc686b863d26384ae7bd3c0bf09
Demirhan Yilal <demirhanyilal0@gmail.com>
2024-09-08 21:01:28 +0300

    Feat: MessageRepository has been created.

--- messaging_app/src/main/java/org/spring_app/messaging_app/repository/MessageRepository.java
+++ messaging_app/src/main/java/org/spring_app/messaging_app/repository/MessageRepository.java
@@ -1,9 +1,13 @@
 package org.spring_app.messaging_app.repository;
 
 import org.spring_app.messaging_app.entity.Message;
+import org.spring_app.messaging_app.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
+
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    Message findByDateAndSender(Timestamp date, User sender);
 }
