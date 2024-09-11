77281efdb093eb747f78c4a6c849fe883cfcc16e
Demirhan Yilal <demirhanyilal0@gmail.com>
2024-09-11 15:07:24 +0300

    Feat: Query has been written to get messages from db with sender and receiver

--- messaging_app/src/main/java/org/spring_app/messaging_app/repository/MessageReceivedUserBridgeRepository.java
+++ messaging_app/src/main/java/org/spring_app/messaging_app/repository/MessageReceivedUserBridgeRepository.java
@@ -1,9 +1,18 @@
 package org.spring_app.messaging_app.repository;
 
 import org.spring_app.messaging_app.entity.MessageReceivedUserBridge;
+import org.spring_app.messaging_app.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MessageReceivedUserBridgeRepository extends JpaRepository<MessageReceivedUserBridge, Long> {
+
+    @Query("SELECT m FROM MessageReceivedUserBridge m WHERE m.receiver = :receiver AND m.message.sender = :sender")
+    List<MessageReceivedUserBridge> findByReceiverAndSender(@Param("receiver") User receiver, @Param("sender") User sender);
 }
+
