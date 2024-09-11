ed3a3199abc544b8f8565a0e62fcf7ba6f6adf8d
Demirhan Yilal <demirhanyilal0@gmail.com>
2024-09-11 12:53:25 +0300

    MessageGetRequest has been created

---
+++ messaging_app/src/main/java/org/spring_app/messaging_app/dto/MessageGetRequest.java
@@ -0,0 +1,15 @@
+package org.spring_app.messaging_app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageGetRequest {
+    String receiver;
+    String sender;
+}
