a667ae033692e21a87fdfdf8000a26a56a7dc444
Demirhan Yilal <demirhanyilal0@gmail.com>
2024-09-16 14:15:44 +0300

    Update: When a message sent and received the authenticated user's nick name have been sended via AuthenticationPrincipal because of security reasons

--- messaging_app/src/main/java/org/spring_app/messaging_app/controller/message/MessageRestController.java
+++ messaging_app/src/main/java/org/spring_app/messaging_app/controller/message/MessageRestController.java
@@ -4,8 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.spring_app.messaging_app.dto.message.MessageDto;
 import org.spring_app.messaging_app.dto.message.MessageGetRequest;
 import org.spring_app.messaging_app.dto.message.MessagePostRequest;
+import org.spring_app.messaging_app.repository.UserRepository;
 import org.spring_app.messaging_app.service.MessageService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,21 +18,30 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MessageRestController {
     private final MessageService messageService;
+    private final UserRepository userRepository;
 
     @PostMapping("/send")
-    public ResponseEntity<MessagePostRequest> sendMessage(@RequestBody MessagePostRequest messagePostRequest) {
-        System.out.println("sender:" + messagePostRequest.getSender() + ". receiver:" + messagePostRequest.getReceiver() + ". content: " +
-                messagePostRequest.getContent() + ". date: " + messagePostRequest.getSqlDate());
+    public ResponseEntity<MessagePostRequest> sendMessage(@AuthenticationPrincipal UserDetails userDetails,
+                                                          @RequestBody MessagePostRequest messagePostRequest) {
+        String sender = userRepository.findByEmail(userDetails.getUsername()).orElseThrow().getNick();
+        messagePostRequest.setSender(sender);
 
-        messageService.saveSentMessage(messagePostRequest);
-        messageService.saveReceivedMessage(messagePostRequest);
+        messageService.saveMessage(messagePostRequest);
         //todo: make error handling
-
         return ResponseEntity.ok(messagePostRequest);
     }
 
     @GetMapping("/get")
-    public ResponseEntity<List<MessageDto>> getMessages(MessageGetRequest messageGetRequest) {
+    public ResponseEntity<List<MessageDto>> getMessages(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @RequestParam("receiver") String receiver) {
+        String sender = userRepository.findByEmail(userDetails.getUsername()).orElseThrow().getNick();
+
+        MessageGetRequest messageGetRequest = MessageGetRequest.builder()
+                .sender(sender)
+                .receiver(receiver)
+                .build();
+
         List<MessageDto> messages = messageService.getReceivedMessages(messageGetRequest);
         return ResponseEntity.ok(messages);
     }
