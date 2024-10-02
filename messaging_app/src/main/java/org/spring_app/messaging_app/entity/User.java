9891174a0e92db7d1a337c851246d7cfdcce206f
Demirhan Yilal <demirhanyilal0@gmail.com>
2024-10-02 14:06:28 +0300

    Update: Necessary fields in user entity has been updated as non nullable

--- messaging_app/src/main/java/org/spring_app/messaging_app/entity/User.java
+++ messaging_app/src/main/java/org/spring_app/messaging_app/entity/User.java
@@ -25,12 +25,13 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String email;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String nick;
 
+    @Column(nullable = false)
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER)
